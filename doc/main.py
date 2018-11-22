daabcaff7b2ca3f7facd8dd63248ef4b47586e9e
taburiss3525 <taburiss3525@gmail.com>
2018-11-21 19:14:20 -0500

    7:15 11/21 CY

--- doc/main.py
+++ doc/main.py
@@ -7,6 +7,8 @@ Created on Mon Nov 19 14:19:48 2018
 
 from pyxdameraulevenshtein import damerau_levenshtein_distance
 from nltk.metrics.distance import edit_distance
+import py_common_subseq.py_common_subseq as CS #Need to change xrange() as range()
+import math
 import pandas as pd
 import sys
 sys.path.append("..")
@@ -17,9 +19,14 @@ Dictionary = pd.read_csv("../output/test_dictionary.csv").word
 Threshold = 3
 We = 'rah'
     
-Candidate = p4.candidate_search(Dictionary, We, Threshold)
-dist_score = p4.distance_score(Candidate, We, Threshold)
+Candidates = p4.candidate_search(Dictionary, We, Threshold)
+dist_score = p4.distance_score(Candidates, We, Threshold)
 
 #------------------------------
 
+a1=0.25
+a2=0.25
+a3=0.25
+a4=0.25
 
+simi_score = p4.similarity_score(Candidates, We, a1, a2, a3, a4)
--- lib/functions.py
+++ lib/functions.py
@@ -6,6 +6,8 @@ Created on Sun Nov 18 15:18:26 2018
 """
 from pyxdameraulevenshtein import damerau_levenshtein_distance
 from nltk.metrics.distance import edit_distance
+import py_common_subseq.py_common_subseq as CS #Need to change xrange() as range()
+import math
 import pandas as pd
 
 # Candidate search
@@ -32,6 +34,56 @@ class project4():
         return(Score)
             
 # String similarity
-#    def similarity_score(candidates, We, a1, a2, a3, a4):
+    def similarity_score(candidates, We, a1=0.25, a2=0.25, a3=0.25, a4=0.25):
+        Score = {}
+        for Wc in candidates:
+            common_subsequences = CS.find_common_subsequences(Wc, We)
+            lcs = sorted(common_subsequences, key=lambda x: len(x))[-1]
+            
+            
+            IniLetter = We[0]
+            EndLetter = We[-1]
+            MidLetter = We[math.ceil(len(We)/2)]
+            
+            #LCS_1
+            common_subseq_IntLetter = set([])
+            for W in common_subsequences:
+                if W.startswith(IniLetter):
+                    common_subseq_IntLetter.add(W)
+                 
+            if len(common_subseq_IntLetter) == 0:
+                lcs1 = ''
+            else:
+                lcs1 = sorted(common_subseq_IntLetter, key=lambda x: len(x))[-1]
+            
+            #LCS_z
+            common_subseq_EndLetter = set([])
+            for W in common_subsequences:
+                if W.endswith(EndLetter):
+                    common_subseq_EndLetter.add(W)
+                    
+            if len(common_subseq_EndLetter) == 0:
+                lcsz = ''
+            else:
+                lcsz = sorted(common_subseq_EndLetter, key=lambda x: len(x))[-1]
+            
+            #LCS_n
+            common_subseq_MidLetter = set([])
+            for W in common_subsequences:
+                if W.startswith(MidLetter):
+                    common_subseq_MidLetter.add(W)
+                    
+            if len(common_subseq_MidLetter) == 0:
+                lcsn = ''
+            else:
+                lcsn = sorted(common_subseq_MidLetter, key=lambda x: len(x))[-1]
+                
+            denom = len(Wc) + len(We)
+            nlcs = (2*len(lcs)**2)/denom
+            nmnlcs1 = (2*len(lcs1)**2)/denom
+            nmnlcsn = (2*len(lcsn)**2)/denom
+            nmnlcsz = (2*len(lcsz)**2)/denom
+            score = a1*nlcs + a2*nmnlcs1 + a3*nmnlcsn + a4*nmnlcsz
+            Score[Wc] = score
         
-        
+        return(Score)
