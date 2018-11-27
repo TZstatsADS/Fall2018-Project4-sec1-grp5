b752bce45d3691a2f3042bb92ff39827f8a437f1
taburiss3525 <taburiss3525@gmail.com>
2018-11-27 17:38:25 -0500

    5:38 11/27 CY

--- doc/main.py
+++ doc/main.py
@@ -48,23 +48,41 @@ simi_score = p4.similarity_score(Candidates, We, a1, a2, a3, a4)
 # data = dict(Counter(data_))
 # data['rch']
 # =============================================================================
-We = "eve"
-Wc = "King"
-test = ["apple bee car dog eve", "bee car dog eve fat", "car dog eve fat get", "dog eve fat get hi", "eve fat get hi ill"]
-grams_e = []
-grams_C_X = []
-for grams in test:
-    grams_e.append(grams.replace(We, Wc, 1))
-for i in range(5):
-    five_gram_s = list(jieba.cut(grams_e[i]))
-    for k in range(5):
-        if -i+4==k:
-            continue
-        else:
-            five_gram_s_copy = copy.deepcopy(five_gram_s)
-            five_gram_s_copy[2*k] = "*"
-            gram_c_x = "".join(five_gram_s_copy)
-            grams_C_X.append(gram_c_x)
-            
-print(grams_C_X)
-        
+# =============================================================================
+# We = "eve"
+# Wc = "King"
+# test = ["apple bee car dog eve", "bee car dog eve fat", "car dog eve fat get", "dog eve fat get hi", "eve fat get hi ill"]
+# grams_e = []
+# grams_C_X = []
+# for grams in test:
+#     grams_e.append(grams.replace(We, Wc, 1))
+# for i in range(5):
+#     five_gram_s = list(jieba.cut(grams_e[i]))
+#     for k in range(5):
+#         if -i+4==k:
+#             continue
+#         else:
+#             five_gram_s_copy = copy.deepcopy(five_gram_s)
+#             five_gram_s_copy[2*k] = "*"
+#             gram_c_x = "".join(five_gram_s_copy)
+#             grams_C_X.append(gram_c_x)
+#             
+# print(grams_C_X)
+# =============================================================================
+# Feature
+Feature = pd.DataFrame()
+Feature["We"] = ["rah"]*10 + ["abc"]*10
+Feature["Y"] = [0]*6+[1]+[0]*8+[1]+[0]*4
+We_df = pd.DataFrame()
+length = len(Candidates)
+We_df["We"] = [We]*10
+
+We_df["dist_score"] = 
+# resample to balance label 1 and label 0
+label_1 = Feature.loc[Feature.Y == 1]
+for i in range(2):
+    label_1 = label_1.append(label_1)
+Feature = Freature.append(label_1)
+
+
+.readli
